8346feca883ba8689fd36b850e476544376a83af
Jonathan <jonathan.max.annett@gmail.com>
2026-02-19 21:01:13 +1100

    align with Theldus issue 115

--- examples/echo/echo.c
+++ examples/echo/echo.c
@@ -19,12 +19,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#ifdef WS_STATICS
-#define WS_STATICS_DATA_IMPLEMENTATION
-#include "echo-static.h"
-#endif
 #include <ws.h>
-
 
 /**
  * @dir examples/
@@ -110,7 +105,7 @@ void onmessage(ws_cli_conn_t client,
 	 *   ws_sendframe_bin()
 	 *   ws_sendframe_bin_bcast()
 	 */
-	ws_sendframe_bcast(8080, (char *)msg, size, type);
+	ws_sendframe_bcast(8080, (const char*)msg, size, type);
 }
 
 /**
@@ -121,11 +116,6 @@ void onmessage(ws_cli_conn_t client,
  */
 int main(void)
 {
-#ifdef WS_STATICS
-	initEmbeddedAssets();
-	sprintf(static_root_alias,"%s","/echo.html");
-
-#endif
 	ws_socket(&(struct ws_server){
 		/*
 		 * Bind host:
